7f9ef460d68bee0763579c59ec688a9bf3ce4272
mahabaraketopenios <mahabaraket.openios@gmail.com>
2025-08-27 15:41:56 +0100

    course solution

--- src/app/modules/what-we-do/components/course/course.component.ts
+++ src/app/modules/what-we-do/components/course/course.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrl: './course.component.scss'
+  styleUrl: './course.component.scss',
 })
 export class CourseComponent {
+  mesured: string[] = [
+    'Increase student response rates with deep SIS ans LMS integration',
 
+    'Automatic evaluation tracking process with 90% of stuf time saved ',
+
+    'sychnonize results distri?? and analyse ?? over time ',
+
+    '?? student learnings and surveys evaluation process',
+
+    ' rich reporting to ? and record ?? and achivements without intensive timming',
+  ];
 }
