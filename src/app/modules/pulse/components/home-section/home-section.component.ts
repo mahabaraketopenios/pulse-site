cf9eb36b90f084e32707d61917a58acee08c6db7
mahabaraketopenios <mahabaraket.openios@gmail.com>
2025-09-10 16:03:04 +0100

    upate home sections

--- src/app/modules/pulse/components/home-section/home-section.component.ts
+++ src/app/modules/pulse/components/home-section/home-section.component.ts
@@ -13,25 +13,25 @@ export class HomeSectionComponent {
     {
       question: 'D1. Specification Layer',
       answer:
-        'CodePen is an online community for testing and showcasing HTML, CSS, and JavaScript code snippets.',
+        'Quis nostrud cillum consectetur incididunt tempor irure pariatur ea culpa. Duis officia ad sunt anim duis velit enim minim velit velit ad. Elit id culpa aliqua sit occaecat sit laboris consectetur laboris sunt consequat reprehenderit. Elit fugiat consectetur exercitation ullamco deserunt nisi nostrud aute nisi excepteur.',
       open: false,
     },
     {
       question: 'D2. Assessment Layer',
       answer:
-        'Yes, CodePen has a free plan with many features, and a Pro plan with advanced capabilities.',
+        'Nisi incididunt adipisicing reprehenderit tempor cillum laboris. Anim veniam duis tempor duis ut sint qui nisi eu cupidatat. Pariatur commodo dolor tempor veniam incididunt exercitation ad elit aliquip eu in. Quis dolore elit nostrud culpa laborum nulla. Adipisicing incididunt eiusmod laboris in irure qui minim excepteur voluptate eu dolore ex ullamco officia. In nisi dolore mollit in veniam et occaecat. Cupidatat occaecat velit laborum voluptate dolor.',
       open: false,
     },
     {
       question: 'D3. Reporting Layer',
       answer:
-        'Absolutely! You can share your pens with a unique URL or embed them into your website.',
+        'Exercitation sit sit reprehenderit nostrud ad Lorem deserunt laborum enim eu sunt cupidatat. Laborum veniam sint dolore deserunt pariatur dolore. Deserunt incididunt ea anim proident. Esse anim fugiat occaecat nisi aliquip voluptate. Voluptate in commodo aliqua sint dolor quis reprehenderit. Non et mollit ullamco commodo officia ullamco laboris laborum minim ex eiusmod. Consequat esse Lorem non ex esse laborum sunt.',
       open: false,
     },
     {
       question: 'D4. Faculty Progress Monitoring',
       answer:
-        'Responsive design ensures your website looks and functions well on all devices, improving user experience and SEO rankings.',
+        'Ut id sunt qui aliqua. Sunt et id adipisicing sunt sunt id fugiat voluptate dolore aliquip. In excepteur ea elit labore fugiat velit laborum nulla magna cupidatat. Non labore ad non elit Lorem veniam adipisicing.',
       open: false,
     },
   ];
