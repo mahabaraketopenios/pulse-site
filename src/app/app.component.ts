250ec629e28e40459546117354fb2576c8c22659
mahabaraketopenios <mahabaraket.openios@gmail.com>
2025-09-11 09:39:08 +0100

    update AOS (animation init)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import * as AOS from 'aos';
 import { SharedModule } from './modules/shared/shared.module';
@@ -11,16 +11,27 @@ import { SharedModule } from './modules/shared/shared.module';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit() {
-    if (isPlatformBrowser(this.platformId)) {
+    /* if (isPlatformBrowser(this.platformId)) {
       AOS.init({
         once: true, // Run only once
         startEvent: 'load', // Trigger when page is fully loaded
+        
       });
+    } */
+  }
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init({
+        once: false,
+        // duration: 1000,
+      });
+      AOS.refresh(); // 👈 important
     }
   }
 }
--- src/app/modules/pulse/components/home/home.component.ts
+++ src/app/modules/pulse/components/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import Aos from 'aos';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    if (isPlatformBrowser(this.platformId)) {
+      Aos.init({
+        once: false,
+        // duration: 1000,
+      });
+    }
+  }
 }
